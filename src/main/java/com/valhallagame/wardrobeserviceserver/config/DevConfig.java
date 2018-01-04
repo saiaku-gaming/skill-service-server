eb7b908e13388f37c65d154593642021cdc93285
Mio Engel <mioengel@gmail.com>
2018-01-04 21:21:09 +0100

    Added dev configs

--- src/main/java/com/valhallagame/wardrobeserviceserver/config/DevConfig.java
+++ src/main/java/com/valhallagame/wardrobeserviceserver/config/DevConfig.java
@@ -1,5 +1,22 @@
 package com.valhallagame.wardrobeserviceserver.config;
 
-public class DevConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
+import com.valhallagame.characterserviceclient.CharacterServiceClient;
+import com.valhallagame.personserviceclient.PersonServiceClient;
+
+@Configuration
+@Profile("default")
+public class DevConfig {
+	@Bean
+	public CharacterServiceClient characterServiceClient() {
+		return CharacterServiceClient.get();
+	}
+
+	@Bean
+	public PersonServiceClient personServiceClient() {
+		return PersonServiceClient.get();
+	}
 }
