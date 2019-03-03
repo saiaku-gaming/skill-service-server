d0c18cf0a70ce41cec786442731d31616014208a
Mio Engel <mioengel@gmail.com>
2019-03-03 23:56:41 +0100

    More MDC fixing

--- src/main/java/com/valhallagame/traitserviceserver/config/RabbitMQConfig.java
+++ src/main/java/com/valhallagame/traitserviceserver/config/RabbitMQConfig.java
@@ -1,18 +1,24 @@
 package com.valhallagame.traitserviceserver.config;
 
+import com.valhallagame.common.rabbitmq.RabbitMQRouting;
+import com.valhallagame.common.rabbitmq.RabbitSender;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.valhallagame.common.rabbitmq.RabbitMQRouting;
-
 @Configuration
 public class RabbitMQConfig {
+
+	@Autowired
+	private RabbitTemplate rabbitTemplate;
+
 	// Trait configs
 	@Bean
 	public DirectExchange characterExchange() {
@@ -22,6 +28,11 @@ public class RabbitMQConfig {
 	@Bean
 	public DirectExchange featExchange() {
 		return new DirectExchange(RabbitMQRouting.Exchange.FEAT.name());
+	}
+
+	@Bean
+	public DirectExchange traitExchange() {
+		return new DirectExchange(RabbitMQRouting.Exchange.TRAIT.name());
 	}
 
 	@Bean
@@ -55,5 +66,10 @@ public class RabbitMQConfig {
 		SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
 		factory.setMessageConverter(jacksonConverter());
 		return factory;
+	}
+
+	@Bean
+	public RabbitSender rabbitSender() {
+		return new RabbitSender(rabbitTemplate);
 	}
 }
--- src/main/java/com/valhallagame/traitserviceserver/rabbitmq/NotificationConsumer.java
+++ src/main/java/com/valhallagame/traitserviceserver/rabbitmq/NotificationConsumer.java
@@ -5,12 +5,15 @@ import com.valhallagame.traitserviceserver.model.Trait;
 import com.valhallagame.traitserviceserver.service.TraitService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.UUID;
 
 @Component
 public class NotificationConsumer {
@@ -18,24 +21,43 @@ public class NotificationConsumer {
 	private static final Logger logger = LoggerFactory.getLogger(NotificationConsumer.class);
 
 	@Autowired
-	TraitService traitService;
+	private TraitService traitService;
+
+	@Value("${spring.application.name}")
+	private String appName;
 
 	@RabbitListener(queues = { "#{traitCharacterDeleteQueue.name}" })
 	public void receiveCharacterDelete(NotificationMessage message) {
+		MDC.put("service_name", appName);
+		MDC.put("request_id", message.getData().get("requestId") != null ? (String)message.getData().get("requestId") : UUID.randomUUID().toString());
+
 		logger.info("Received character delete notification with message {}", message);
-		String characterName = (String) message.getData().get("characterName");
-		List<Trait> traits = traitService.getTraits(characterName);
-		for (Trait trait : traits) {
-			traitService.deleteTrait(trait);
+
+		try {
+			String characterName = (String) message.getData().get("characterName");
+			List<Trait> traits = traitService.getTraits(characterName);
+			for (Trait trait : traits) {
+				traitService.deleteTrait(trait);
+			}
+		} finally {
+			MDC.clear();
 		}
 	}
 
 	@RabbitListener(queues = { "#{traitFeatAddQueue.name}" })
 	public void receiveFeatAdd(NotificationMessage message) throws IOException {
-		logger.info("Received fead add notification with message: {}", message);
-		String featName = (String) message.getData().get("feat");
-		String characterName = (String) message.getData().get("characterName");
+		MDC.put("service_name", appName);
+		MDC.put("request_id", message.getData().get("requestId") != null ? (String)message.getData().get("requestId") : UUID.randomUUID().toString());
 
-		traitService.handleFeatAdding(characterName, featName);
+		logger.info("Received fead add notification with message: {}", message);
+
+		try {
+			String featName = (String) message.getData().get("feat");
+			String characterName = (String) message.getData().get("characterName");
+
+			traitService.handleFeatAdding(characterName, featName);
+		} finally {
+			MDC.clear();
+		}
 	}
 }
